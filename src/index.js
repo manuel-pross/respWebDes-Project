9778ecf86c246162a85fe8e577612c4cd38edd26
manuel-pross <pro_manuel@yahoo.de>
2021-06-07 11:13:19 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import "./styles/main.scss";
+
+import $ from "jquery";
+import "slick-carousel";
+
+console.log("hi");
+console.log("hello");
+
+$('.header__carousel').slick({
+    centerMode: true,
+    arrows: false
+});
