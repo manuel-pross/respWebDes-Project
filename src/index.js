8d87b6763a1e4a08419ae08d3aed4b6bdc0f3dc8
manuel-pross <pro_manuel@yahoo.de>
2021-06-08 13:59:55 +0200

    add slider arrows

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import "slick-carousel";
 console.log("hi");
 console.log("hello");
 
-$('.header__carousel').slick({
+$(".header__carousel").slick({
     centerMode: true,
     arrows: true,
+    prevArrow:
+        "<div class='slick-arrow-prev'><span class='arrow-content-prev'></span></div>",
+    nextArrow:
+        "<div class='slick-arrow-next'><span class='arrow-content-next'></span></div>",
     responsive: [
         {
-          breakpoint: 768,
-          settings: {
-              arrows: false,
-          }
-        }
-      ]
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+            },
+        },
+    ],
 });
