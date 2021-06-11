51073a8b120e14f764e214365b8030f7e38083ca
manuel-pross <pro_manuel@yahoo.de>
2021-06-11 21:00:43 +0200

    WIP: first version of wallpaper slider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import "./styles/main.scss";
 import $ from "jquery";
 import "slick-carousel";
 
-console.log("hi");
-console.log("hello");
-
 $(".header__carousel").slick({
+    centerMode: true,
+    arrows: true,
+    prevArrow:
+        "<div class='slick-arrow-prev'><span class='arrow-content-prev'></span></div>",
+    nextArrow:
+        "<div class='slick-arrow-next'><span class='arrow-content-next'></span></div>",
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+            },
+        },
+    ],
+});
+
+$(".photo-wall__carousel").slick({
     centerMode: true,
     arrows: true,
     prevArrow:
